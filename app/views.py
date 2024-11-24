235dbf8f53d1b3003dbfebdf4e966bec8fa75393
Yeddula <yedduladastagiri999@gmail.com>
2024-11-24 20:43:48 +0530

    initial commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here. 
+from django.http import HttpResponse  
+
+def atchyuth(request):
+    return HttpResponse('atchyuth Is A Innocent boy who doesnt know')
